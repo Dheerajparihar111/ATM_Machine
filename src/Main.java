ad21a70f3cf55668aaba175e55d90145206b407c
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-02-02 22:37:02 +0530

    added simple input and output statement

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.*;
+class options{
+    void options(){
+        System.out.println("Enter your choice");
+        System.out.println("1)Add Money");
+        System.out.println("2) Withdraw Money ");
+        System.out.println("3) EXIT");
+    }
+}
+public class Main {
+    static void main(String[] args) {
+        System.out.println("*********Wel-Come to ATM Machine***********");
+        Scanner input = new Scanner(System.in);
+        options Options = new options();
+        Options.options();
+        int choice = input.nextInt();
+        switch (choice){
+            case 1:
+                    System.out.println("Enter the Amount");
+                    float creditAmount = input.nextFloat();
+                    System.out.println("Money Added Successfully");
+                    break;
+            case 2:
+                System.out.println("Enter The Amount");
+                float debitAmount = input.nextFloat();
+                System.out.println(debitAmount+"$"+" withdrawal successfully");
+                break;
+            case 3:
+                System.out.println("Thank You For Using ATM Machine");
+                 System.exit(0);
+        }
+    }
 }
